5e56ce96aefe34418fe2b2242933d40e26b2a12e
nohosh <sanchit8b@gmail.com>
2018-12-17 14:57:07 +0530

    recipe module controller/getrecipe()/calcTime()/calcServing()/render recipe

--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -22,8 +22,8 @@ export default class Recipe {
     calcTime() {
         //Assuming we need 15 min for every 3 ingredients
         const numIng = this.ingredients.length;
-        const periods = Math.ceil(numIng/3);
-        this.time = periods*15;
+        const periods = Math.ceil(numIng / 3);
+        this.time = periods * 15;
     }
 
     calcServings() {
