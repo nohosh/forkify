99e23017d76210a1a1ff0fb6d7118f947fcd52da
nohosh <sanchit8b@gmail.com>
2018-12-20 16:00:54 +0530

    searchView added and rendered the result to the page. few recipes are not rendering

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
-import * as searchView from './views/searchView'
+import * as searchView from './views/searchView';
+import * as recipeView from './views/recipeView';
 import { elements, renderLoader, clearLoader } from './views/base';
 
 /** Global state of the app 
@@ -72,7 +73,9 @@ const controlRecipe = async () => {
     
     if (id){
         // Prepare UI for changes
-
+        recipeView.clearRecipe();
+        renderLoader(elements.recipe);
+        
         // Create new recipe obj
         state.recipe = new Recipe(id);
        
@@ -88,6 +91,8 @@ const controlRecipe = async () => {
             state.recipe.calcServings();
 
             // Render recipe
+            clearLoader();
+            recipeView.renderRecipe(state.recipe);
             console.log(state.recipe);
         }catch(err) {
             alert('Error processing recipe!');
